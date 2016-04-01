fcb645c37cafbb1e95b5b656a9e71c7d94f61baa
mta-dev1 <marnold@oxfordsciences.com>
2016-04-01 15:10:33 +0100

    first time
    initial commit from eclipse

---
+++ src/HelloMe.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author marnold
+ *
+ */
+public class HelloMe {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String x = "Hello World";
+		x += "a bit more stuff too";
+		System.out.println(x);
+	}
+
+}
