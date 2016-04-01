8f562f184ce869899c3ac74a776d059bcdf7becc
mta-dev2 <matt@marnold.net>
2016-04-01 15:14:15 +0100

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub which now has an edit
 		String x = "Hello World";
 		x += "a bit more stuff too";
 		System.out.println(x);
